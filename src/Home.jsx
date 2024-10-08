bddfa730a24b84655682a77b0486209cdd15105d
kennedfer <kenned14santos@gmail.com>
2024-10-08 19:52:52 -0300

    feat: Change the view to test TodoList page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,33 @@
 // Redenerize seus componentes aqui...
 
-import { Login } from './components/login/login';
-import './index.css';
+import { Login } from "./components/login/login";
+import "./index.css";
+import TodoList from "./pages/TodoList";
 
 const Home = () => {
-    return (
-        <>
-            <main>
-                <Login />
-            </main>
-        </>
-    );
+  const apiFakeData = [
+    {
+      produto: "Coquinha gelada",
+      validade: "2028",
+      prioridade: true,
+    },
+    {
+      produto: "Arroz paia",
+      validade: "2028",
+      prioridade: false,
+    },
+  ];
+
+  return (
+    <>
+      <main>
+        {/* <Login />
+         */}
+
+        <TodoList apiData={apiFakeData} />
+      </main>
+    </>
+  );
 };
 
 export { Home };
